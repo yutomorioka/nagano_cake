ad9171b775e00628fa8456148adf357427fb3a82
EC2 Default User <ec2-user@ip-172-31-36-3.ap-northeast-1.compute.internal>
2023-07-26 04:13:11 +0000

    Initial Commit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,44 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:update] = "You have updated user info successfully."
+      redirect_to customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
+  def unsubscribe
+    @customer = current_customer
+  end
+
+
+  def withdraw
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "Thank you for the good rating. We hope to see you again."
+　　redirect_to root_path
+  end
+
+
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name,
+                                     :last_name_kana, :first_name_kana,
+                                     :email, :postal_code, :address, :telephone_number)
+  end
+
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,11 @@
 class Public::HomesController < ApplicationController
+  
+  def top
+    @items = Item.all
+    @random = Item.order("RANDOM()").limit(4)
+  end
+
+  def about
+  end
+  
 end
