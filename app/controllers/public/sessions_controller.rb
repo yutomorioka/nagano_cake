d7922905cdd99acc8b970e9493343684f420d2cc
EC2 Default User <ec2-user@ip-172-31-36-3.ap-northeast-1.compute.internal>
2023-07-29 13:50:18 +0000

    Initial Commit

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +24,20 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+    mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+  return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
+      redirect_to new_customer_session_path
+    end
+  end
+  
 end
